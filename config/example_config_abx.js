723386f53d8cf9c05daff11fead34d425a86bf67
Gurjaspal <gurjaspal.bedi@gmail.com>
2020-05-13 22:05:40 -0400

    Changed the header and added the new configuration file.

--- config/example_config_abx.js
+++ config/example_config_abx.js
@@ -1,6 +1,6 @@
 // configure the test here
 var TestConfig = {
-  "TestName": "ABX Demo Test",
+  "TestName": "ABX Test",
   "LoopByDefault": true,
   "ShowFileIDs": false,
   "ShowResults": false,
@@ -11,22 +11,167 @@ var TestConfig = {
   "AudioRoot": "",
   "Testsets": [
     //    
-    {
-      "Name": "Schubert",
-      "TestID": "id1",
-      "Files": {
-        "A": "audio/schubert_ref.wav",
-        "B": "audio/schubert_2.wav",
-      }
-    },
-    //    
-    {
-      "Name": "Castanets",
-      "TestID": "id2",
-      "Files": {
-        "A": "audio/castanets_ref.wav",
-        "B": "audio/castanets_2.wav",
-      }
-    },
+        {
+        	"Name":"Test1",
+        	"TestID":"1",
+        	"Files":{
+        		"A": "audio/8kbps_prop_0_Base.wav",
+        		"B": "audio/8kbps_prop_0_Prop.wav",
+        		"X": "audio/8kbps_prop_0_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test2",
+        	"TestID":"2",
+        	"Files":{
+        		"A": "audio/8kbps_prop_1_Base.wav",
+        		"B": "audio/8kbps_prop_1_Prop.wav",
+        		"X": "audio/8kbps_prop_1_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test3",
+        	"TestID":"3",
+        	"Files":{
+        		"A": "audio/8kbps_prop_2_Prop.wav",
+        		"B": "audio/8kbps_prop_2_Base.wav",
+        		"X": "audio/8kbps_prop_2_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test4",
+        	"TestID":"4",
+        	"Files":{
+        		"A": "audio/8kbps_prop_3_Base.wav",
+        		"B": "audio/8kbps_prop_3_Prop.wav",
+        		"X": "audio/8kbps_prop_3_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test5",
+        	"TestID":"5",
+        	"Files":{
+        		"A": "audio/8kbps_prop_4_Prop.wav",
+        		"B": "audio/8kbps_prop_4_Base.wav",
+        		"X": "audio/8kbps_prop_4_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test6",
+        	"TestID":"6",
+        	"Files":{
+        		"A": "audio/8kbps_prop_5_Base.wav",
+        		"B": "audio/8kbps_prop_5_Prop.wav",
+        		"X": "audio/8kbps_prop_5_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test7",
+        	"TestID":"7",
+        	"Files":{
+        		"A": "audio/16kbps_prop_0_Base.wav",
+        		"B": "audio/16kbps_prop_0_Prop.wav",
+        		"X": "audio/16kbps_prop_0_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test8",
+        	"TestID":"8",
+        	"Files":{
+        		"A": "audio/16kbps_prop_1_Prop.wav",
+        		"B": "audio/16kbps_prop_1_Base.wav",
+        		"X": "audio/16kbps_prop_1_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test9",
+        	"TestID":"9",
+        	"Files":{
+        		"A": "audio/16kbps_prop_2_Prop.wav",
+        		"B": "audio/16kbps_prop_2_Base.wav",
+        		"X": "audio/16kbps_prop_2_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test10",
+        	"TestID":"10",
+        	"Files":{
+        		"A": "audio/16kbps_prop_3_Prop.wav",
+        		"B": "audio/16kbps_prop_3_Base.wav",
+        		"X": "audio/16kbps_prop_3_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test11",
+        	"TestID":"11",
+        	"Files":{
+        		"A": "audio/16kbps_prop_4_Base.wav",
+        		"B": "audio/16kbps_prop_4_Prop.wav",
+        		"X": "audio/16kbps_prop_4_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test12",
+        	"TestID":"12",
+        	"Files":{
+        		"A": "audio/16kbps_prop_5_Base.wav",
+        		"B": "audio/16kbps_prop_5_Prop.wav",
+        		"X": "audio/16kbps_prop_5_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test13",
+        	"TestID":"13",
+        	"Files":{
+        		"A": "audio/24kbps_prop_0_Base.wav",
+        		"B": "audio/24kbps_prop_0_Prop.wav",
+        		"X": "audio/24kbps_prop_0_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test14",
+        	"TestID":"14",
+        	"Files":{
+        		"A": "audio/24kbps_prop_1_Base.wav",
+        		"B": "audio/24kbps_prop_1_Prop.wav",
+        		"X": "audio/24kbps_prop_1_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test15",
+        	"TestID":"15",
+        	"Files":{
+        		"A": "audio/24kbps_prop_2_Prop.wav",
+        		"B": "audio/24kbps_prop_2_Base.wav",
+        		"X": "audio/24kbps_prop_2_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test16",
+        	"TestID":"16",
+        	"Files":{
+        		"A": "audio/24kbps_prop_3_Prop.wav",
+        		"B": "audio/24kbps_prop_3_Base.wav",
+        		"X": "audio/24kbps_prop_3_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test17",
+        	"TestID":"17",
+        	"Files":{
+        		"A": "audio/24kbps_prop_4_Base.wav",
+        		"B": "audio/24kbps_prop_4_Prop.wav",
+        		"X": "audio/24kbps_prop_4_X.wav"
+        	}
+        },
+        {
+        	"Name":"Test18",
+        	"TestID":"18",
+        	"Files":{
+        		"A": "audio/24kbps_prop_5_Prop.wav",
+        		"B": "audio/24kbps_prop_5_Base.wav",
+        		"X": "audio/24kbps_prop_5_X.wav"
+        	}
+        }
   ]
 }
